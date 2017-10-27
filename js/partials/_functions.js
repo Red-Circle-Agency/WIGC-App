6cccdfc8f99847834ede690d240f5e6bbbe023ab
Brian Henderson <brianhenderson@Brians-MacBook-Air.local>
2017-10-27 13:09:13 -0500

    Removed piwik from functions for future merge.

--- js/partials/_functions.js
+++ js/partials/_functions.js
@@ -1,17 +1,4 @@
 // jshint -W117
-
-var _paq = _paq || [];
-/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-_paq.push(['trackPageView']);
-_paq.push(['enableLinkTracking']);
-(function() {
-  var u="//circle.red/stats/";
-  _paq.push(['setTrackerUrl', u+'piwik.php']);
-  _paq.push(['setSiteId', '2']);
-  var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-  g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
-})();
-
 function snap(array, search_term) {
   for (var i = array.length - 1; i >= 0; i--) {
     if (array[i] === search_term) {
